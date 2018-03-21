4773889a9f1c756d0af5623a6e68d5f91f5543d6
arapiku <arpaiku420@gmail.com>
2018-03-21 15:57:33 +0900

    ログイン失敗時のエラー収集

---
+++ app/execptions/LoginHttpActionAdapter.java
@@ -0,0 +1,31 @@
+package execptions;
+
+import org.pac4j.core.context.*;
+import org.pac4j.play.PlayWebContext;
+import org.pac4j.play.http.DefaultHttpActionAdapter;
+import play.mvc.Result;
+
+import static play.mvc.Controller.flash;
+import static play.mvc.Results.redirect;
+
+/**
+ * ログインエラーのハンドリング
+ *
+ * @author arapiku
+ */
+public class LoginHttpActionAdapter extends DefaultHttpActionAdapter {
+
+    @Override
+    public Result adapt(int status, PlayWebContext ctx) {
+        if (status == HttpConstants.UNAUTHORIZED) {
+            ctx.getJavaContext().session().remove(Pac4jConstants.SESSION_ID);
+            flash("failed", "login.errors.401");
+            return redirect("/");
+        } else if (status == HttpConstants.FORBIDDEN) {
+            flash("failed", "login.errors.403");
+            return redirect("/");
+        } else {
+            return super.adapt(status, ctx);
+        }
+    }
+}

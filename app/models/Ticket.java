345b025b648749d9ef7261e4155a8ba04c227bf4
arapiku <arpaiku420@gmail.com>
2018-03-31 16:55:06 +0900

    Ticketエンティティの型と修飾子の修正

--- app/models/Ticket.java
+++ app/models/Ticket.java
@@ -3,6 +3,7 @@ package models;
 import com.fasterxml.jackson.annotation.JsonIgnore;
 
 import javax.persistence.*;
+import java.time.LocalDateTime;
 import java.util.*;
 
 /**
@@ -25,10 +26,10 @@ public class Ticket {
     public String body;
 
     /** 授業開始時間 */
-    public Date startTime;
+    public LocalDateTime startTime;
 
     /** 授業終了時間 */
-    public Date endTime;
+    public LocalDateTime endTime;
 
     /** 価格 */
     public Integer price;
@@ -40,11 +41,82 @@ public class Ticket {
     /** レビュー情報 */
     @OneToMany(cascade = CascadeType.ALL, mappedBy = "ticket")
     @JsonIgnore
-    public List<UserReview> userReviews;
+    private List<UserReview> userReviews;
 
     /** チケット情報 */
     @OneToMany(cascade = CascadeType.ALL, mappedBy = "ticket")
     @JsonIgnore
-    public List<PurchasedTicket> purchasedTickets;
+    private List<PurchasedTicket> purchasedTickets;
 
+    public Long getTicketId() {
+        return ticketId;
+    }
+
+    public void setTicketId(Long ticketId) {
+        this.ticketId = ticketId;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getBody() {
+        return body;
+    }
+
+    public void setBody(String body) {
+        this.body = body;
+    }
+
+    public LocalDateTime getStartTime() {
+        return startTime;
+    }
+
+    public void setStartTime(LocalDateTime startTime) {
+        this.startTime = startTime;
+    }
+
+    public LocalDateTime getEndTime() {
+        return endTime;
+    }
+
+    public void setEndTime(LocalDateTime endTime) {
+        this.endTime = endTime;
+    }
+
+    public Integer getPrice() {
+        return price;
+    }
+
+    public void setPrice(Integer price) {
+        this.price = price;
+    }
+
+    public User getUser() {
+        return user;
+    }
+
+    public void setUser(User user) {
+        this.user = user;
+    }
+
+    public List<UserReview> getUserReviews() {
+        return userReviews;
+    }
+
+    public void setUserReviews(List<UserReview> userReviews) {
+        this.userReviews = userReviews;
+    }
+
+    public List<PurchasedTicket> getPurchasedTickets() {
+        return purchasedTickets;
+    }
+
+    public void setPurchasedTickets(List<PurchasedTicket> purchasedTickets) {
+        this.purchasedTickets = purchasedTickets;
+    }
 }
